6b613b7f4f9b520cfab1d2d25f69a0986cfd0db8
EasyH <skysea001010@gmail.com>
2026-07-16 12:16:10 +0900

    kotlin-reflect 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation(kotlin("stdlib-jdk8"))
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
 
 tasks.withType<Test> {
